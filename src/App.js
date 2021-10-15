65d46576388adfff1b788bbfd36920382f5c1d63
Sayro <sayrodamaceno70@gmail.com>
2021-10-15 20:18:33 -0300

    exercicio do dia 1 do bloco 12

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const load = 'https://a-static.mlcdn.com.br/1500x1500/adesivo-parede-decorativo-loading-carregando-palavra-barra-aartedecor/aartedecor/1541-1863/bbc74def4e1116a12776398372101bcd.jpg';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dogLink: '',
+      loading: true,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  handleClick() {
+    this.fetchDog();
+  }
+
+  async fetchDog() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const fetchDog = await fetch('https://dog.ceo/api/breeds/image/random');
+        const response = await fetchDog.json();
+        this.setState({
+          dogLink: response.message,
+          loading: false,
+        });
+      },
+    );
+  }
+
+  render() {
+    const { dogLink, loading } = this.state;
+    return (
+      <div>
+        <h1>Random Dog</h1>
+        <div className="dog-card">
+          {loading ? <img src={ load } alt="loading" />
+            : <img src={ dogLink } alt="dog" />}
+          <button onClick={ this.handleClick } type="button">New Dog</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
